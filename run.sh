3a1576c17bdc787c1ad3b829eb9787c529bc6fba
M D <mirco.deckers@gmail.com>
2025-08-30 08:37:52 +0200

    add data for mar-2024 dec-2024

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")" && echo "📁 Changed dir to $(pwd)" || { echo "❌ ERROR: Could not change dir or no dir provided"; exit 1; }
 
-ods_export /opt/bin/data/workout/reps.ods --sheet=km --range=A17:D28 --no-header --output="data.csv"
+function create_and_append_dump() {
+	ods_export /opt/bin/data/workout/reps.ods --sheet=km --range=$1 --no-header --output="data_dump.csv"
+	cat data_dump.csv >> data.csv
+	rm data_dump.csv
+}
+
+echo -n "" > data.csv
+create_and_append_dump "A6:D15"
+create_and_append_dump "A17:D28"
