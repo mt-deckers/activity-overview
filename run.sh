e662f3778a3818028928ac1df41eecd51133a42e
M D <mirco.deckers>
2025-12-03 11:45:38 +0100

    output mount

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-docker build -t workout-extractor .
+#docker build -t workout-extractor .
 
-docker run --rm -v "$PWD":/data workout-extractor \
-    --file /data/reps.ods \
-    --sheet 2025 --range A1:D12 \
-    --sheet 2024 --range A1:D12 \
-    --sheet 2023 --range A1:D1 \
-    --out /data/data.json
-
+docker run --rm \
+  -v /opt/bin/data/workout/reps.ods:/input/reps.ods \
+  -v $(pwd)/output:/output \
+  workout-extractor \
+  --file /input/reps.ods \
+  --out /output/data.json \
+  --sheet 2025 --range A1:D12 \
+  --sheet 2024 --range A1:D12 \
+  --sheet 2023 --range A1:D1
