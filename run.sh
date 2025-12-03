23cc093f10a170a0faf4ea14383f6e41a90a6928
M D <mirco.deckers>
2025-12-03 12:12:40 +0100

    unfuck EVERYTHING

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ docker run --rm \
   -v /opt/bin/data/workout/reps.ods:/input/reps.ods \
   -v $(pwd)/output:/output \
   workout-extractor \
-  --file /input/reps.ods \
-  --out /output/data.json \
-  --sheet 2025 --range A1:D12 \
+  --sheet 2023 --range A1:D1 \
   --sheet 2024 --range A1:D12 \
-  --sheet 2023 --range A1:D1
+  --sheet 2025 --range A1:D12 \
+  --file /input/reps.ods \
+  --out /output/data.json
 
 if [ $? -eq 0 ]; then
   echo "SUCCESS!!!!"
