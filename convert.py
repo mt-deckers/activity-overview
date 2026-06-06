3ca742ce5b74bf1b1e285f0ff8cfe2c917b38daa
M D <mirco.deckers>
2026-06-06 20:30:30 +0200

    wow python >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[...] js

--- convert.py
+++ convert.py
@@ -3,18 +3,30 @@
 import yaml
 import json
 
-with open("data/data.yml") as f:
+with open("data.yml") as f:
     yml_data = yaml.safe_load(f)
 
 # build years
 # labels
 data = {
-    "years": set([
-        key.split("-")[0] if "-" in key else None for key, value in yml_data.items()
-    ]),
+    "years": sorted(
+        set(
+            [
+                key.split("-")[0] if "-" in key else None
+                for key, value in yml_data.items()
+            ]
+        )
+    ),
+    "months": sorted(yml_data.keys()),
+    "monthly": {
+        "walked": [value["walked"] for key, value in yml_data.items()],
+        "ran": [value["ran"] for key, value in yml_data.items()],
+        "cycled": [value["cycled"] for key, value in yml_data.items()]
+    },
 }
 
 print(yml_data)
+print("===")
 print(data)
 
 with open("data.json", "w") as f:
